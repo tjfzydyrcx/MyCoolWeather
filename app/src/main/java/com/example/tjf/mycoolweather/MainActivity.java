8135e26eaf6dbf47a7543d418e555b57c1eba2ce
tjfzydyrcx <1436862541@qq.com>
2017-12-22 19:59:21 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/tjf/mycoolweather/MainActivity.java
+++ app/src/main/java/com/example/tjf/mycoolweather/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.tjf.mycoolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//拉去成功
 public class MainActivity extends AppCompatActivity {
 
     @Override
